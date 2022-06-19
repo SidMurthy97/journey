2d7c6f4806a40900cc53bf1a944a864a0e89fbbf
Sid Murthy <murthy.siddhant@googlemail.com>
2022-06-19 20:45:45 +0100

    proof of concept working. Able to call gmaps api to get time to destination

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,1 +1,23 @@
 '''This script tests whether I can actually retrieve the data that I want'''
+
+import os
+from turtle import home 
+import googlemaps
+from datetime import datetime
+import pprint
+#first get the API key 
+api_key = os.getenv("JOURNEY_KEY")
+home_address = os.getenv("JOURNIFY_HOME_ADDRESS")
+work_address = os.getenv("JOURNIFY_WORK_ADDRESS")
+
+#authenciate
+gmaps = googlemaps.Client(key=api_key)
+
+now = datetime.now()
+directions_result = gmaps.directions("21 Selwyn Road, CB39EA",
+                                     "Evonetix",
+                                     mode="driving",
+                                     departure_time=now)
+
+pprint.pprint(directions_result)
+
